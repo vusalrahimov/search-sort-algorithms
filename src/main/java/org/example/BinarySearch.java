cfa5f654030f5513ae194064011adcdb38025736
vusal <vusalr06@gmail.com>
2023-06-16 16:13:46 +0400

    Initial commit

---
+++ src/main/java/org/example/BinarySearch.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = (right + left)/2;
+        int result = -1;
+
+        while (left<=right) {
+            if (arr[mid]<key) {
+                left = mid + 1;
+            } else if (arr[mid]==key) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                right = mid - 1;
+            }
+            mid = (left + right) / 2;
+        }
+
+        return result;
+    }
+
+}
