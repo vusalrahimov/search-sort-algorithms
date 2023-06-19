a8cf52a843ddde7a4adb2df885717ddc0a85ccab
vusal <vusalr06@gmail.com>
2023-06-19 21:23:34 +0400

    SelectionSort was added

---
+++ src/main/java/org/example/sort/SelectionSort.java
@@ -0,0 +1,27 @@
+package org.example.sort;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length;j++) {
+               if (arr[j] < min) {
+                   min = arr[j];
+                   minIndex = j;
+               }
+            }
+
+            if (minIndex != 0) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+        }
+
+    }
+
+}
